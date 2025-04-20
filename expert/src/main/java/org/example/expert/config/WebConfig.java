cd0139fbe57feac60185f5fd1653993bcd1baee9
ParkSungGyu1 <steven0507@naver.com>
2025-04-20 21:13:58 +0900

    Lv3

--- expert/src/main/java/org/example/expert/config/WebConfig.java
+++ expert/src/main/java/org/example/expert/config/WebConfig.java
@@ -1,8 +1,10 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.config.interceptor.AdminAccessInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,5 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
+    }
+
+    // Interceptor 등록
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminAccessInterceptor())
+                .addPathPatterns("/admin/**");
     }
 }
